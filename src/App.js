1899d5a090bedd3ed24848f71e63031c0fd726ce
zaikh847 <zaikh847@student.liu.se>
2022-04-10 02:35:26 +0500

    Setup files for admin panel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path="new" element={<New />} />
+              <Route path=":userId" element={<Single />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path="new" element={<New />} />
+              <Route path=":productId" element={<Single />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
