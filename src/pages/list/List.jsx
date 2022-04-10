a4b86a237924406375a31d050a0c0fe058613aa2
zaikh847 <zaikh847@student.liu.se>
2022-04-10 22:10:32 +0500

    Add datatable for users and products

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,16 @@
 import "./list.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
 
 const List = () => {
   return (
-    <div>
-      <h1>List</h1>
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
     </div>
   );
 };
